b643ffbaadb4cc23be6d184474675d4b50b20fd4
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-06 15:15:33 +0200

    Update GPS_Config.h

--- GPS/GPS_Config.h
+++ GPS/GPS_Config.h
@@ -8,14 +8,14 @@
 #define GPS_GPGGA_Size	72
 
 typedef struct{
-	u8 Data			[GPS_Array_Size];
-	u8 GPGGA		[GPS_GPGGA_Size];
+	u8 Data		[GPS_Array_Size];
+	u8 GPGGA	[GPS_GPGGA_Size];
 	u8 Longitude	[9];
-	u8 Latitude		[10];
-	u8 Altitude		[6];
-	u8 Time			[12];
+	u8 Latitude	[10];
+	u8 Altitude	[6];
+	u8 Time		[12];
 	u8 Satellite_No	[3];
-	u8 HDOP			[5];
+	u8 HDOP		[5];
 }GPS_Frames;
 
 #endif /* GPS_CONFIG_H_ */
