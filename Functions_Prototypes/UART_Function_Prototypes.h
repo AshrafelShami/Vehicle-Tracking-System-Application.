88be00ea3b079017a6f9f5b4f9af5d9dd1703078
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-06 15:14:05 +0200

    Update UART_Function_Prototypes.h

--- Functions_Prototypes/UART_Function_Prototypes.h
+++ Functions_Prototypes/UART_Function_Prototypes.h
@@ -1,12 +1,12 @@
 
 #include "../DataTypes/DataTypes.h"
 
-u8	USART_Receive			( void );
-u8 USART_Receive_NoBlock	( void );
-u16 USART_Receive_9bit		( void );
+u8      USART_Receive		( void );
+u8      USART_Receive_NoBlock	( void );
+u16     USART_Receive_9bit	( void );
 
-void USART_Transmit			( u8 Send_Data );
+void USART_Transmit		( u8 Send_Data );
 void USART_Transmit_String	( u8* Array_String );
 void USART_Transmit_9Bit	( u16 Data_9bit );
 
-void USART_Disable			( void );
+void USART_Disable		( void );
