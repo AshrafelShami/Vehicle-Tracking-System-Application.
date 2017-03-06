9ac45180d4101a1a02d603c9d9e92d6f74737cb3
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-06 21:22:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -45,11 +45,11 @@ int main(void){
 void System_Init( void ){
 	/*		USART Initialization			*/
 	Struct_USART USART_Init_Struct;
-	USART_Init_Struct.BuadRate		=	9600;
+	USART_Init_Struct.BuadRate	=	9600;
 	USART_Init_Struct.Operation_Mode=	Asynchronous_Normal_Speed;
 	USART_Init_Struct.Duplex_Select	=	TX_RX_Mode;
-	USART_Init_Struct.Char_Size		=	Char_8_Bits;
-	USART_Init_Struct.Stop_Bit		=	One_Bit;
+	USART_Init_Struct.Char_Size	=	Char_8_Bits;
+	USART_Init_Struct.Stop_Bit	=	One_Bit;
 	USART_Init_Struct.Parity_Mode	=	No_Parity;
 	USART_Init(USART_Init_Struct);
 
