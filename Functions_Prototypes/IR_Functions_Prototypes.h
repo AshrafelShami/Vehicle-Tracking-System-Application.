737fe4f92774ab383fc8466750a54a4b22821972
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-06 15:13:29 +0200

    Update IR_Functions_Prototypes.h

--- Functions_Prototypes/IR_Functions_Prototypes.h
+++ Functions_Prototypes/IR_Functions_Prototypes.h
@@ -3,7 +3,7 @@
 #define IR_FUNCTIONS_PROTOTYPES_H_
 #include "../DataTypes/DataTypes.h"
 
-void IR_Sensors_Init( void );
-u8 Accident_Check	( void );
+void IR_Sensors_Init    ( void );
+u8 Accident_Check       ( void );
 
 #endif /* IR_FUNCTIONS_PROTOTYPES_H_ */
