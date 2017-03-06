4bfce8b80efa38511dc64ce39ee3681de4c1fbdf
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-06 15:08:01 +0200

    Add files via upload

---
+++ SIM900/SIM900_Config.h
@@ -0,0 +1,12 @@
+#ifndef SIM900_CONFIG_H_
+#define SIM900_CONFIG_H_
+#include "../DataTypes/DataTypes.h"
+#include "../Functions_Prototypes/SIM900_Functions_Prototypes.h"
+
+#ifndef F_CPU
+#define F_CPU	16000000ul
+#include <util/delay.h>
+#endif
+
+
+#endif /* SIM900_CONFIG_H_ */
