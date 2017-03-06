e558cfa59c94c538f25b982f946bd8c34a682ebb
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-06 15:23:24 +0200

    Update UART_Config.c

--- UART/UART_Config.c
+++ UART/UART_Config.c
@@ -25,17 +25,17 @@ void USART_Init(Struct_USART USART_Config){
 	UCSR0C &=~(1 << UMSEL0);
 
 	switch(USART_Config.Operation_Mode){
-		case Synchronous				: UCSR0C |= (1U << UMSEL0); break;
+		case Synchronous		: UCSR0C |= (1U << UMSEL0); break;
 		case Asynchronous_Normal_Speed	: UCSR0C &=~(1U << UMSEL0); UCSR0A &=~(1U << U2X0); break;
 		case Asynchronous_Double_Speed	: UCSR0C &=~(1U << UMSEL0); UCSR0A |= (1U << U2X0); break;
-		default							: UCSR0C &=~(1U << UMSEL0); UCSR0A &=~(1U << U2X0); break;
+		default				: UCSR0C &=~(1U << UMSEL0); UCSR0A &=~(1U << U2X0); break;
 	}
 	
 	switch(USART_Config.Duplex_Select){
 		case TX_Mode	: UCSR0B |= (1U << TXEN0); break;
 		case RX_Mode	: UCSR0B |= (1U << RXEN0); break;
 		case TX_RX_Mode : UCSR0B |= (1U << TXEN0)|(1U << RXEN0); break;
-		default			: UCSR0B |= (1U << TXEN0); break; 
+		default		: UCSR0B |= (1U << TXEN0); break; 
 	}
 	
 	switch(USART_Config.Char_Size){
@@ -44,33 +44,33 @@ void USART_Init(Struct_USART USART_Config){
 		case Char_7_Bits : UCSR0B &=~(1U << UCSZ02); UCSR0C |= (1U << UCSZ01); UCSR0C &=~(1U << UCSZ00); break;
 		case Char_8_Bits : UCSR0B &=~(1U << UCSZ02); UCSR0C |= (1U << UCSZ01); UCSR0C |= (1U << UCSZ00); break;
 		case Char_9_Bits : UCSR0B |= (1U << UCSZ02); UCSR0C |= (1U << UCSZ01); UCSR0C |= (1U << UCSZ00); break;
-		default			 : UCSR0B &=~(1U << UCSZ02); UCSR0C |= (1U << UCSZ01); UCSR0C |= (1U << UCSZ00); break;
+		default		 : UCSR0B &=~(1U << UCSZ02); UCSR0C |= (1U << UCSZ01); UCSR0C |= (1U << UCSZ00); break;
 	}
 	
 	switch(USART_Config.Parity_Mode){
 		case No_Parity	: UCSR0C &=~(1U << UPM01); UCSR0C &=~(1U << UPM00); break;
 		case Even_Parity: UCSR0C |= (1U << UPM01); UCSR0C &=~(1U << UPM00); break;
 		case Odd_Parity	: UCSR0C |= (1U << UPM01); UCSR0C |= (1U << UPM00); break;
-		default			: UCSR0C &=~(1U << UPM01); UCSR0C &=~(1U << UPM00); break;
+		default		: UCSR0C &=~(1U << UPM01); UCSR0C &=~(1U << UPM00); break;
 	}
 	
 	switch(USART_Config.Stop_Bit){
 		case One_Bit	: UCSR0C &=~(1U << USBS0); break;
 		case Two_Bits	: UCSR0C |= (1U << USBS0); break;
-		default			: UCSR0C &=~(1U << USBS0); break;
+		default		: UCSR0C &=~(1U << USBS0); break;
 	}
 
 	switch(USART_Config.Clock_Polirity){
 		case TX_Falling_Edge	: UCSR0C |= (1U << UCPOL0); break;
-		case TX_Rising_Edge		: UCSR0C &=~(1U << UCPOL0); break;
-		default					: UCSR0C &=~(1U << UCPOL0); break;
+		case TX_Rising_Edge	: UCSR0C &=~(1U << UCPOL0); break;
+		default			: UCSR0C &=~(1U << UCPOL0); break;
 	}
 	
 	switch(USART_Config.interrupt_Sel){
-		case RX_Complete_Int_Enable		: UCSR0B |= (1U << RXCIE0); break;
-		case TX_Complete_Int_Enable		: UCSR0B |= (1U << TXCIE0); break;
+		case RX_Complete_Int_Enable	: UCSR0B |= (1U << RXCIE0); break;
+		case TX_Complete_Int_Enable	: UCSR0B |= (1U << TXCIE0); break;
 		case Data_Reg_Empty_Int_Enable	: UCSR0B |= (1U << UDRIE0); break;
-		default							: UCSR0B &=~((1U << RXCIE0)|(1U << TXCIE0)|(1U << UDRIE0)); break;
+		default				: UCSR0B &=~((1U << RXCIE0)|(1U << TXCIE0)|(1U << UDRIE0)); break;
 	}
 }
 
@@ -110,12 +110,18 @@ Struct_Status USART_Rx_Error_Detection( void ){
 ********************************************************************************************/
 u8 USART_Receive( void ){
 	while (!(UCSR0A & (1U << RXC0)));	/*	Wait for the data to be received			*/
-	return UDR0;						/*	Return the received data in the UDR0		*/
+	return UDR0;				/*	Return the received data in the UDR0			*/
 }
 
+/********************************************************************************************
+  Name:     USART_Receive.
+  Purpose:  Function receive the data of the UDR0 register without polling.
+  Entry:    no parameters
+  Exit:     ( return UDR0 ) Return Data in unsigned char.
+********************************************************************************************/
 u8 USART_Receive_NoBlock( void ){
-	if (UCSR0A & (1U << RXC0))			/*	Check for the data to be received without polling	*/
-		return UDR0;					/*	Return the received data in the UDR0				*/
+	if (UCSR0A & (1U << RXC0))		/*	Check for the data to be received without polling	*/
+		return UDR0;			/*	Return the received data in the UDR0			*/
 	else
 		return 0;
 }
@@ -142,7 +148,7 @@ u16 USART_Receive_9bit( void ){
 ********************************************************************************************/
 void USART_Transmit( u8 Data ){
 	while (!(UCSR0A & (1U << UDRE0)));	/*	Wait for the Buffer register to be Free.(1)	*/	
-	UDR0 = Data;						/*	Send the Data to the Data register.			*/
+	UDR0 = Data;				/*	Send the Data to the Data register.		*/
 }
 
 /********************************************************************************************
@@ -176,5 +182,5 @@ void USART_Transmit_String( u8* String ){
   Exit:     no parameters
 ********************************************************************************************/
 void USART_Disable( void ){
-	UCSR0B &=~((1 << TXEN0)|(1 << RXEN0));	/*	Disable Transmitter and Receiver.			*/
+	UCSR0B &=~((1 << TXEN0)|(1 << RXEN0));	/*	Disable Transmitter and Receiver.	*/
 }
